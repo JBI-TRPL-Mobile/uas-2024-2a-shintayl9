b8de60b4cb612d5ab899e3e525ba2d7a917ef13f
Shinta Yulistiana <shintayulistiana2004@gmail.com>
2025-01-09 14:39:57 +0700

    rute untuk home page dan navigasi ke halaman lain

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'provider/auth_provider.dart';
 import 'screen/landing_page.dart';
+import 'screen/sign_in.dart';
+import 'screen/sign_up.dart';
+import 'screen/home_page.dart';
+import 'screen/messages.dart';
+import 'screen/profile.dart';
 
 void main() {
   runApp(
@@ -18,12 +23,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'WIREFRAME PROJECT',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-      ),
-      home: LandingPage(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'WIREFRAME PROJECT',
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => LandingPage(),
+          '/signin': (context) => SignIn(),
+          '/signup': (context) => SignUp(),
+          '/home': (context) => HomePage(),
+          '/messages': (context) => Messages(),
+          '/profile': (context) => Profile(),
+        });
   }
 }
